9257f3b28f35cd27e3ed319ac0f3c5f05ca2b3ed
Adam Baldwin <evilpacket@github.com>
2020-07-17 10:55:34 -0700

    stuff and things

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const core = require('@actions/core');
-const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
     const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString()) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString())
-
+    console.log(ms)
+    console.log(process.env)
     core.setOutput('time', new Date().toTimeString());
   } 
   catch (error) {
